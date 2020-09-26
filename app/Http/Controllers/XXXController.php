2c3a598c4066bdff714a24a32145838fe4895b10
gmarutani <genki.marutani@rakuten-sec.co.jp>
2020-09-26 20:21:40 +0900

    first commit

---
+++ app/Http/Controllers/XXXController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class XXXController extends Controller
+{
+    public function add()
+  {
+    return view('admin.news.create');
+}
+}
